c54d66d2615b632c543bdd0c2cfe8ce1ea259d4d
Filip Rec <filiprec@outlook.com>
2015-11-12 16:32:04 +0100

    Initial commit

---
+++ django_simple_search2/__init__.py
@@ -0,0 +1,1 @@
+from utils import search_handler
---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import os
+import sys
+from setuptools import setup, find_packages
+
+version = '0.0.1'
+
+setup(
+    name='django-simple-search2',
+    version=version,
+    description='Django simple search module.',
+    author='Filip Rec',
+    author_email='filiprec@outlook.com',
+    url='https://github.com/zefj/django-simple-search2',
+    zip_safe=False,
+    packages=['django_simple_search2'],
+    
+    install_requires=[
+        'Django',
+    ],
+    include_package_data=True,
+)
